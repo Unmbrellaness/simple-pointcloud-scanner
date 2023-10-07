e6c5102e08c06373b02ae2772c3718624c9c5b9c
Unmbrellaness <74270804+Unmbrellaness@users.noreply.github.com>
2023-10-07 20:33:08 +0800

    Add files via upload

---
+++ HARDWARE/TIMER/timer.c
@@ -0,0 +1,76 @@
+#include "timer.h"
+#include "led.h"
+#include "usart.h"
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTE精英STM32开发板
+//定时器 驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2012/9/4
+//版本：V1.1
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
+//All rights reserved									  
+//********************************************************************************
+//V1.1 20120904
+//1,增加TIM3_PWM_Init函数。
+//2,增加LED0_PWM_VAL宏定义，控制TIM3_CH2脉宽									  
+//////////////////////////////////////////////////////////////////////////////////  
+   	  
+//通用定时器3中断初始化
+//这里时钟选择为APB1的2倍，而APB1为36M
+//arr：自动重装值。
+//psc：时钟预分频数
+//这里使用的是定时器3!
+
+
+
+
+//TIM3 PWM部分初始化 
+//PWM输出初始化
+//arr：自动重装值
+//psc：时钟预分频数
+void TIM3_PWM_Init(u16 arr,u16 psc)
+{  
+	GPIO_InitTypeDef GPIO_InitStructure;
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef  TIM_OCInitStructure;
+	
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);	//使能定时器3时钟
+ 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);  //使能GPIO外设和AFIO复用功能模块时钟
+ 
+   //设置该引脚为复用输出功能,输出TIM3 CH2的PWM脉冲波形	GPIOB.5
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1; //TIM_CH2
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;  //复用推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化GPIO
+ 
+   //初始化TIM3
+	TIM_TimeBaseStructure.TIM_Period = arr-1; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值
+	TIM_TimeBaseStructure.TIM_Prescaler =psc-1; //设置用来作为TIMx时钟频率除数的预分频值 
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0; //设置时钟分割:TDTS = Tck_tim
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM向上计数模式
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure); //根据TIM_TimeBaseInitStruct中指定的参数初始化TIMx的时间基数单位
+	
+	//初始化TIM4 Channe34 PWM模式	 
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1; //选择定时器模式:TIM脉冲宽度调制模式2
+ 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //比较输出使能
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High; //输出极性:TIM输出比较极性高
+
+
+	
+	TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+	TIM_OC4Init(TIM3, &TIM_OCInitStructure);
+	
+
+	TIM_OC3PreloadConfig(TIM3,TIM_OCPreload_Enable);
+	TIM_OC4PreloadConfig(TIM3,TIM_OCPreload_Enable);
+ 
+ 	TIM_CtrlPWMOutputs(TIM3, ENABLE);
+	
+	TIM_Cmd(TIM3, ENABLE);  //使能TIM3
+	
+
+}
